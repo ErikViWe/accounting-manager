dc6429cc4088174ae1fcdf753bf2a5c5ab034bb7
ErikViWe <erik.weinstock@googlemail.com>
2016-12-21 16:32:30 +0100

    Implemented new button line into the settings frame.

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -4,9 +4,12 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
+import settings.components.SettingsBottomButtons;
+
 public class Settings {
 	
 	private JFrame settingsFrame;
+	private SettingsBottomButtons settingsBottomButtons;
 	
 	public Settings() {
 		init();
@@ -18,6 +21,9 @@ public class Settings {
 		settingsFrame.setTitle("Settings");
 		settingsFrame.setLayout(new BorderLayout());
 		settingsFrame.setVisible(true);
+		
+		settingsBottomButtons = new SettingsBottomButtons();
+		settingsFrame.add(settingsBottomButtons, BorderLayout.SOUTH);
 	}
 
 }
