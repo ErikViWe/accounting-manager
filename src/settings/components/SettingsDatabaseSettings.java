845a46594d7c1ce12d392f53dd722250be6e7f59
ErikViWe <erik.weinstock@googlemail.com>
2017-01-04 13:17:57 +0100

    Changed style of labels and text-fields.

--- src/settings/components/SettingsDatabaseSettings.java
+++ src/settings/components/SettingsDatabaseSettings.java
@@ -1,11 +1,14 @@
 package settings.components;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
@@ -41,29 +44,38 @@ public class SettingsDatabaseSettings {
 	 */
 	private void init() {
 		frame = new JFrame();
-		frame.setSize(300, 400);
-		frame.setLayout(new BoxLayout(frame, BoxLayout.X_AXIS));
+		frame.setSize(300, 250);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
 		frame.setTitle("Database settings");
+		
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
 		lblUser = new JLabel("Username:");
 		frame.add(lblUser);
 		tfUser = new JTextField();
 		frame.add(tfUser);
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
 		lblPassword = new JLabel("Password:");
 		frame.add(lblPassword);
 		tfPassword = new JPasswordField();
 		frame.add(tfPassword);
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
 		lblIP = new JLabel("IP:");
 		frame.add(lblIP);
 		tfIP = new JTextField();
 		frame.add(tfIP);
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
 		lblPort = new JLabel("Port:");
 		frame.add(lblPort);
 		tfPort = new JTextField();
 		frame.add(tfPort);
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
 		btnCancel = new JButton("Cancel");
 		frame.add(btnCancel);
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
 		btnSave = new JButton("Save");
 		frame.add(btnSave);
+		frame.add(Box.createRigidArea(new Dimension(10, 10)));
+		
 		frame.setVisible(true);
 	}
 	
