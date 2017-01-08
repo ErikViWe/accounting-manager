9b72d8a0f2581f23bed05688d147ba143fb8199f
ErikViWe <erik.weinstock@googlemail.com>
2017-01-08 14:01:38 +0100

    Added method to set initial settings Values.

--- src/helperMethods/SettingsFileExistHelper.java
+++ src/helperMethods/SettingsFileExistHelper.java
@@ -1,6 +1,8 @@
 package helperMethods;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -15,24 +17,21 @@ public class SettingsFileExistHelper {
 	 * Checks if settings-file exists. If not, it will create a new settings-file
 	 */
 	public static void checkForExistence() {
-		//get path to .jar-file
-		File f = new File(System.getProperty("java.class.path"));
-		File dir = f.getAbsoluteFile().getParentFile();
-		String path = dir.toString();
+		String path = getSettingsFilePath();
 		//check if file exists and create new file if settings are missed
-		if (!new File(path + "settings.txt").exists()) {
+		if (!new File(path).exists()) {
 			createSettingsFile(path);
 		}
 	}
 	
 	/**
-	 * Creates a new txt-file at the given path.
-	 * The settings fill will contain initial values.
+	 * Creates a new txt-file at the given path
+	 * The settings fill will contain initial values
 	 * @param path
 	 * 		path where to place the new settings-file
 	 */
 	public static void createSettingsFile(String path) {
-		File f = new File(path + "/settings.txt");
+		File f = new File(path);
 		f.getParentFile().mkdirs();
 		
 		try {
@@ -40,6 +39,37 @@ public class SettingsFileExistHelper {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//TODO fill new file with initial values
+		resetSettingsFile();
+	}
+	
+	/**
+	 * Fills the settings-file with initial values
+	 */
+	public static void resetSettingsFile() {
+		FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter(getSettingsFilePath());
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write("username\n");
+			bufferedWriter.write("password\n");
+			bufferedWriter.write("000.000.000.000\n");
+			bufferedWriter.write("3306");
+			bufferedWriter.close();
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Gets the current path of the settings-file
+	 * @return
+	 * 		path to settings-file
+	 */
+	private static String getSettingsFilePath() {
+		//get path to .jar-file
+		File f = new File(System.getProperty("java.class.path"));
+		File dir = f.getAbsoluteFile().getParentFile();
+		return dir.toString() + "/settings.txt";
 	}
 }
