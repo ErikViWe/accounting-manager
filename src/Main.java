c2fcd8dbdc962dfe0bab59ce5a866ab7053266e2
ErikViWe <erik.weinstock@googlemail.com>
2017-01-08 14:13:31 +0100

    RenamedSettingsFileExistHelper to SettingsHelper.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import gui.GUI;
-import helperMethods.SettingsFileExistHelper;
+import helperMethods.SettingsHelper;
 
 
 public class Main {
@@ -11,7 +11,7 @@ public class Main {
 	 * @version 1.0
 	 */
 	public static void main(String[] args) {
-		SettingsFileExistHelper.checkForExistence();
+		SettingsHelper.checkForExistence();
 		GUI mainGUI = new GUI();
 	}
 
